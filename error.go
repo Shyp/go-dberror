b1056e321b8e71cc8994bb91d41c989e0c0fbc48
Kevin Burke <burke@shyp.com>
2016-02-07 22:15:29 -0800

    support check constraints
    unfortunately the error message here is less helpful so let people register
    their error constraints

--- error.go
+++ error.go
@@ -10,6 +10,7 @@ import (
 const (
 	CodeInvalidTextRepresentation = "22P02"
 	CodeNotNullViolation          = "23502"
+	CodeCheckViolation            = "23514"
 )
 
 type DBError struct {
@@ -19,11 +20,23 @@ type DBError struct {
 	Severity   string
 	Routine    string
 	Table      string
+	Detail     string
 	Column     string
 }
 
 func (dbe *DBError) Error() string {
 	return dbe.Message
+}
+
+type Constraint struct {
+	Name     string
+	GetError func(*pq.Error) *DBError
+}
+
+var constraintMap = map[string]Constraint{}
+
+func RegisterConstraint(c Constraint) {
+	constraintMap[c.Name] = c
 }
 
 // GetDBError parses a given database error and returns a human-readable
@@ -34,25 +47,39 @@ func GetDBError(err error) error {
 	}
 	switch pqerr := err.(type) {
 	case *pq.Error:
-		{
-			fmt.Printf("%#v\n", pqerr)
-			if pqerr.Code == CodeInvalidTextRepresentation {
-				msg := strings.Replace(pqerr.Message, "invalid input syntax for", "Invalid input syntax for type", -1)
+		switch pqerr.Code {
+		case CodeInvalidTextRepresentation:
+			msg := strings.Replace(pqerr.Message, "invalid input syntax for", "Invalid input syntax for type", -1)
+			return &DBError{
+				Message:  msg,
+				Code:     string(pqerr.Code),
+				Severity: pqerr.Severity,
+			}
+		case CodeNotNullViolation:
+			msg := fmt.Sprintf("No %[1]s was provided. Please provide a %[1]s", pqerr.Column)
+			return &DBError{
+				Message:  msg,
+				Code:     string(pqerr.Code),
+				Column:   pqerr.Column,
+				Table:    pqerr.Table,
+				Severity: pqerr.Severity,
+			}
+		case CodeCheckViolation:
+			fmt.Printf("%#v\n", (pqerr))
+			c, ok := constraintMap[pqerr.Constraint]
+			if ok {
+				return c.GetError(pqerr)
+			} else {
 				return &DBError{
-					Message:  msg,
-					Code:     string(pqerr.Code),
-					Severity: pqerr.Severity,
-				}
-			} else if pqerr.Code == CodeNotNullViolation {
-				msg := fmt.Sprintf("No %[1]s was provided. Please provide a %[1]s", pqerr.Column)
-				return &DBError{
-					Message:  msg,
-					Code:     string(pqerr.Code),
-					Column:   pqerr.Column,
-					Table:    pqerr.Table,
-					Severity: pqerr.Severity,
+					Message:    pqerr.Message,
+					Code:       string(pqerr.Code),
+					Column:     pqerr.Column,
+					Table:      pqerr.Table,
+					Severity:   pqerr.Severity,
+					Constraint: pqerr.Constraint,
 				}
 			}
+		default:
 			return &DBError{
 				Message:    pqerr.Message,
 				Code:       string(pqerr.Code),
@@ -66,5 +93,4 @@ func GetDBError(err error) error {
 	default:
 		return pqerr
 	}
-	return nil
 }
--- error_test.go
+++ error_test.go
@@ -6,7 +6,11 @@ import (
 	"testing"
 
 	"github.com/letsencrypt/boulder/test"
+	"github.com/lib/pq"
 )
+
+const uuid = "3c7d2b4a-3fc8-4782-a518-4ce9efef51e7"
+const email = "test@example.com"
 
 var db *sql.DB
 
@@ -32,15 +36,39 @@ func setUp(t *testing.T) {
 
 func TestNotNull(t *testing.T) {
 	setUp(t)
-	err := db.QueryRow("INSERT INTO accounts (id) VALUES (null)").Scan()
+	_, err := db.Exec("INSERT INTO accounts (id) VALUES (null)")
 	dberr := GetDBError(err)
 	switch e := dberr.(type) {
 	case *DBError:
-		{
-			test.AssertEquals(t, e.Error(), "No id was provided. Please provide a id")
-			test.AssertEquals(t, e.Column, "id")
-			test.AssertEquals(t, e.Table, "accounts")
-		}
+		test.AssertEquals(t, e.Error(), "No id was provided. Please provide a id")
+		test.AssertEquals(t, e.Column, "id")
+		test.AssertEquals(t, e.Table, "accounts")
+	default:
+		t.Fail()
+	}
+}
+
+func TestConstraint(t *testing.T) {
+	setUp(t)
+	constraint := Constraint{
+		Name: "accounts_balance_check",
+		GetError: func(e *pq.Error) *DBError {
+			return &DBError{
+				Message:  "Cannot write a negative balance",
+				Severity: e.Severity,
+				Table:    e.Table,
+				Detail:   e.Detail,
+				Code:     string(e.Code),
+			}
+		},
+	}
+	RegisterConstraint(constraint)
+	_, err := db.Exec("INSERT INTO accounts (id, email, balance) VALUES ($1, $2, -1)", uuid, email)
+	dberr := GetDBError(err)
+	switch e := dberr.(type) {
+	case *DBError:
+		test.AssertEquals(t, e.Error(), "Cannot write a negative balance")
+		test.AssertEquals(t, e.Table, "accounts")
 	default:
 		t.Fail()
 	}
@@ -53,9 +81,7 @@ func TestInvalidUUID(t *testing.T) {
 	dberr := GetDBError(err)
 	switch e := dberr.(type) {
 	case *DBError:
-		{
-			test.AssertEquals(t, e.Error(), "Invalid input syntax for type uuid: \"foo\"")
-		}
+		test.AssertEquals(t, e.Error(), "Invalid input syntax for type uuid: \"foo\"")
 	default:
 		t.Fail()
 	}
